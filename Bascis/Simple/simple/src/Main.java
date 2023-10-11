6d0c34fadc86c69a854b8971b39f7a5236a6a966
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-11 14:29:33 +0600

    update

--- Bascis/Simple/simple/src/Main.java
+++ Bascis/Simple/simple/src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Date now = new Date();
+        System.out.println("now: "+ now);
+
     }
 }
