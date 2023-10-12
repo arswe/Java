3f1cbd7e7050a6441b6847954206b17d62a64e3d
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-12 11:25:12 +0600

    update

--- Object-oriented Programming/Classes/ClassesAndObjects/src/Main.java
+++ Object-oriented Programming/Classes/ClassesAndObjects/src/Main.java
@@ -1,17 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        var textBox1 = new TextBox();  // textBox1 is a reference to a TextBox object
+        textBox1.setText("Box 1"); // this is a reference to the current object
+        System.out.println(textBox1.text); //
 
-        TextBox textBox1 = new TextBox(); // textBox1 is a reference to a TextBox object
-        textBox1.setText("Box 1");
-
-        System.out.println(textBox1.text.toUpperCase());
-
-        TextBox textBox2 = textBox1; // textBox2 is a reference to the same TextBox object
-        textBox2.setText("Box 2");
-
-        System.out.println(textBox1.text);
-
-        textBox1.clear(); // textBox1 is a reference to a TextBox object
-        System.out.println(textBox1.text);
     }
 }
--- Object-oriented Programming/Classes/ClassesAndObjects/src/TextBox.java
+++ Object-oriented Programming/Classes/ClassesAndObjects/src/TextBox.java
@@ -1,13 +1,13 @@
 public class TextBox {
-    public String text = "";
+    public String text; // field
 
     public void setText(String text) {
-        this.text = text;
+        this.text = text; // this is a reference to the current object
     }
 
     // clear() is a method that doesn't return anything
     public void clear() {
-        text = "";
+        text = ""; // this is a reference to the current object
     }
 }
 
