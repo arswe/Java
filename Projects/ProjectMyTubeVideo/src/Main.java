2e7109fdee4dd32f7d28ffebf2a211f9a89468bf
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-12 08:11:52 +0600

    update

--- Projects/ProjectMyTubeVideo/src/Main.java
+++ Projects/ProjectMyTubeVideo/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         var video = new Video();
         video.setFileName("birthday.mp4");
         video.setTitle("Jennifer's birthday");
-        video.setUser(new User("akaid@domain.com"));
+        video.setUser(new User("swe@domain.com"));
 
         var processor = new VideoProcessor(
             new XVideoEncoder(),
