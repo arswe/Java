35d2335a10fa462e5da227f6a2944d0843572522
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-12 08:57:09 +0600

    update

--- Fundamentals/types/Constants/src/Main.java
+++ Fundamentals/types/Constants/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        // constant variable
         final float PI = 3.14f;
         float radius = 7.5f;
         float area = PI * radius * radius;
