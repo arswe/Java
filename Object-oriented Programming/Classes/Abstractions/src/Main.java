6c525ee280af1a5ed1679483bc6dd5a808db89a1
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-13 21:37:49 +0600

    Abstraction

--- Object-oriented Programming/Classes/Abstractions/src/Main.java
+++ Object-oriented Programming/Classes/Abstractions/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         var employee = new Employee();
+        employee.setBaseSalary(50_000);
         int wage = employee.calculateWage(10);
         System.out.println(wage);
     }
