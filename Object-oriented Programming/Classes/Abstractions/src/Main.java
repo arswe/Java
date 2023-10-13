384da4820e93d7605576b90b3fce4ffbceb918c2
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-13 21:35:47 +0600

    Abstraction

---
+++ Object-oriented Programming/Classes/Abstractions/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        var employee = new Employee();
+        int wage = employee.calculateWage(10);
+        System.out.println(wage);
+    }
+}
