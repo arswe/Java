0610be9704e759a820b6eb25bfa81ce2d8a4817c
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-14 09:28:00 +0600

    Object Class

--- Object-oriented Programming/Inheritances/OverridingMethods/src/Main.java
+++ Object-oriented Programming/Inheritances/OverridingMethods/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         var textBox = new TextBox();
+        textBox.setText("Hello World");
         System.out.println(textBox.toString());
     }
 }
--- Object-oriented Programming/Inheritances/OverridingMethods/src/TextBox.java
+++ Object-oriented Programming/Inheritances/OverridingMethods/src/TextBox.java
@@ -6,7 +6,7 @@ public class TextBox extends UIControl {
         System.out.println("TextBox");
     }
 
-    @Override
+    @Override // Annotation
     public String toString() {
         return text;
     }
