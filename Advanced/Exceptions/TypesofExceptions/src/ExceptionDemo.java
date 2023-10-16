bbe356c23725ecadab4b5469030cc7a2ebdf597e
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-16 22:17:45 +0600

    Types of Exceptions

---
+++ Advanced/Exceptions/TypesofExceptions/src/ExceptionDemo.java
@@ -0,0 +1,7 @@
+import java.io.FileReader;
+
+public class ExceptionDemo {
+    public static void show(){
+        var reader = new FileReader("file.txt"); // name of exception is FileNotFoundException
+    }
+}
---
+++ Advanced/Exceptions/TypesofExceptions/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        // Types of Exceptions
+        // 1. Checked Exceptions
+        //    i) IOException
+        //   ii) SQLException
+        //  iii) ClassNotFoundException
+        //  iv) InvocationTargetException
+        //   v) IllegalAccessException
+        //  vi) NoSuchMethodException
+        // vii) NoSuchFieldException
+        //viii) InterruptedException
+
+        // 2. Unchecked Exceptions (Runtime Exceptions)
+        //    i) IllegalArgumentException
+        //   ii) IllegalStateException
+        //  iii) IndexOutOfBoundsException
+        //  iv) NullPointerException
+        //   v) SecurityException
+        //  vi) StackOverflowError
+        // vii) NoClassDefFoundError
+
+        // 3. Errors
+        //    i) StackOverflowError
+        //   ii) OutOfMemoryError
+        //  iii) VirtualMachineError
+        //  iv) AssertionError
+        //   v) LinkageError
+        //  vi) ExceptionInInitializerError
+        // vii) UnknownError
+        //viii) NoClassDefFoundError
+
+    }
+}
