8bf2f89ef21374ac31cc707a7b4fca06db1fd34b
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-16 23:46:39 +0600

    Generics Create

---
+++ Advanced/Generics/GenericsCreate/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        var list = new List();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+        System.out.println(list.get(2));
+    }
+}
