b446f20a60dc86e3ad874da693e2683a918ecea7
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-16 23:09:51 +0600

    Re throws exception

---
+++ Advanced/Exceptions/ReThrowingExceptions/src/ExceptionsDemo.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class ExceptionsDemo {
+    public static void show() throws IOException {
+        var account = new Account();
+        try {
+            account.deposit(-1);
+        } catch (IOException e) {
+            System.out.println("Logging");
+            throw e;
+        }
+    }
+}
