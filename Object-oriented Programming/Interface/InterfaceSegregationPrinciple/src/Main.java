42efcaab565c574788f9d391a00155a4220f67f8
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-16 20:24:47 +0600

    Interface Segregation Principle

--- Object-oriented Programming/Interface/InterfaceSegregationPrinciple/src/Main.java
+++ Object-oriented Programming/Interface/InterfaceSegregationPrinciple/src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello draggable!");
-
+        System.out.println("Interface Segregation Principle");
     }
 }
