7f2be898425fa8b7adbae87c4d58c677f359d6e5
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-16 22:28:36 +0600

    Catching Exceptions

---
+++ Advanced/Exceptions/CatchingExceptions/src/ExceptionDemo.java
@@ -0,0 +1,12 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class ExceptionDemo {
+    public static void show() {
+        try {
+            var reader = new FileReader("file.txt");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
