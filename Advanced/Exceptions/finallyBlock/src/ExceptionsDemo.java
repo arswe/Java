a16892dc13de6da9fa7eb9edcbbc46b758b07148
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-16 22:44:31 +0600

    Finally Exceptions

--- Advanced/Exceptions/finallyBlock/src/ExceptionsDemo.java
+++ Advanced/Exceptions/finallyBlock/src/ExceptionsDemo.java
@@ -1,2 +1,22 @@
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ExceptionsDemo {
+    public static void show() {
+        FileReader reader = null;
+        try {
+            reader = new FileReader("file.txt"); // This is a must, otherwise the file will be locked
+            var value = reader.read(); // This is a must, otherwise the file will be locked
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close(); // This is a must, otherwise the file will be locked
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
 }
