aef62a5497e0b814976b7fd59cb25247750ab7a3
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-17 09:26:56 +0600

    Generic Methods

---
+++ Advanced/Generics/GenericMethods/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        var max = Utils.max(new User(10), new User(20));
+        System.out.println(max);
+    }
+}
---
+++ Advanced/Generics/GenericMethods/src/User.java
@@ -0,0 +1,19 @@
+public class User implements Comparable<User> {
+    private int points;
+
+    public User(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public int compareTo(User other) {
+        // return points - other.points; // Ascending order
+        return other.points - points; // Descending order
+    }
+
+    @Override
+    public String toString() {
+        return "Points: " + points;
+    }
+}
+
