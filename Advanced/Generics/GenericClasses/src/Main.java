8421878520b2b023ca4d94adb33e2cf1d31df3bc
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-17 08:15:40 +0600

    Generic Classes

---
+++ Advanced/Generics/GenericClasses/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        var list = new GenericList<User>();
+        list.add(new User());
+
+        User user = list.get(0);
+
+    }
+}
