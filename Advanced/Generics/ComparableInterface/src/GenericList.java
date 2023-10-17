eb5721aa929792dc2009a962c8b0dc3f461b915b
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-17 09:19:46 +0600

    Comparable Interface

--- Advanced/Generics/ComparableInterface/src/GenericList.java
+++ Advanced/Generics/ComparableInterface/src/GenericList.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class GenericList {
+public class GenericList<T extends Comparable> {
+    private T[] items = (T[]) new Object[10];
+    private int count;
+
+    public void add(T item) {
+        items[count++] = item;
+    }
+
+    public void get(int index) {
+        items[index] = items[index];
+    }
 }
--- Advanced/Generics/ComparableInterface/src/User.java
+++ Advanced/Generics/ComparableInterface/src/User.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class User {
+public class User implements Comparable<User> {
+    private int points;
+
+    // Constructor
+    public User(int points) {
+        this.points = points;
+    }
+
+    // Getter
+    @Override // Override the toString method
+    public int compareTo(User other) {
+        return points - other.points;
+    }
 }
