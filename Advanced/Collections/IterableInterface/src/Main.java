f7d16efd76aec6111f3cc6c3c83dee305e7b1deb
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-18 14:52:54 +0600

    The Iterable Interface

---
+++ Advanced/Collections/IterableInterface/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        var list = new CollectionList<String>();
+
+        for (var item : list) {
+            System.out.println(item);
+
+        }
+    }
+}
