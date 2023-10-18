8d3a3cd38d47dbb588f9f2838dd838aff0ddba55
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-18 20:59:46 +0600

    Anonymous Inner Classes

---
+++ Advanced/Lambda Expressions and Functional Interfaces/AnonymousInnerClasses/src/LambdasDemo.java
@@ -0,0 +1,14 @@
+public class LambdasDemo {
+    public static void show() {
+        greet(new Printer() {
+            @Override
+            public void print(String message) {
+                System.out.println(message);
+            }
+        });
+    }
+
+    public static void greet(Printer printer) {
+        printer.print("Hello world!");
+    }
+}
