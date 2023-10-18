3957d3f308de99dfd5869a28f05d6ee044e176d5
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-18 16:36:30 +0600

    Map Interface

---
+++ Advanced/Collections/MapInterface/src/MapDemo.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapDemo {
+    public static void show() {
+        var c1 = new Customer("a", "e1");
+        var c2 = new Customer("b", "e2");
+
+        Map<String, Customer> map = new HashMap<>();
+        map.put(c1.getEmail(), c1);
+        map.put(c2.getEmail(), c2);
+
+        map.replace("e1", new Customer("a++", "e1"));
+        System.out.println(map);
+
+//        var customer = map.get("e1");
+
+//        var unknown = new Customer("Unknown", "");
+//        var customer = map.getOrDefault("e10", unknown);
+
+//        var exists = map.containsKey("e10");
+//
+//        System.out.println(exists);
+    }
+}
