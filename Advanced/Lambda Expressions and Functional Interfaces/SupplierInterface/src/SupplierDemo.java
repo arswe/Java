442e3af0c88ae434c0ce7a2a411449311469ab6d
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-19 08:09:46 +0600

    The Supplier Interface

---
+++ Advanced/Lambda Expressions and Functional Interfaces/SupplierInterface/src/SupplierDemo.java
@@ -0,0 +1,12 @@
+import java.util.function.Supplier;
+
+public class SupplierDemo {
+    public static void show() {
+        Supplier<Double> getRandom = Math::random;
+        var random = getRandom.get();
+
+        System.out.println(random);
+
+
+    }
+}
