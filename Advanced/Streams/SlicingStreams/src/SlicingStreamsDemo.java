9082ab6c7d83ee5607448960f0a3d12f1e218309
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-20 23:59:33 +0600

    Slicing Streams

--- Advanced/Streams/SlicingStreams/src/SlicingStreamsDemo.java
+++ Advanced/Streams/SlicingStreams/src/SlicingStreamsDemo.java
@@ -6,13 +6,21 @@ public class SlicingStreamsDemo {
         var movies = List.of(
                 new Movie("a", 10),
                 new Movie("b", 15),
-                new Movie("c", 20)
+                new Movie("c", 30)
         );
 
-        Predicate<Movie> isPopular = movie -> movie.getLikes() > 10;
+
+        // 100 movies
+        //  10 movies per page
+        //  3rd pages
+        //  skip(20) = skip((page - 1) * pageSize))
+        //  limit(10) = limit(pageSize)
 
         movies.stream()
-                .filter(isPopular)
+                // .skip(2)
+//                .skip(20)
+//                .limit(10)
+                .takeWhile(movie -> movie.getLikes() < 30)
                 .forEach(movie -> System.out.println(movie.getTitle()));
     }
 }
