ba8ad55a62fef88c4ff367144261643dc4752a66
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-20 23:32:18 +0600

    Create Stream with example

--- Advanced/Streams/StreamCreate/src/StreamDemo.java
+++ Advanced/Streams/StreamCreate/src/StreamDemo.java
@@ -1,10 +1,11 @@
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 
 public class StreamDemo {
     public static void show() {
-        int[] numbers = {1, 2, 3};
-        Arrays.stream(numbers)
-            .forEach(System.out::println);
+        Stream.generate(Math::random)
+                .limit(3)
+                .forEach(System.out::println);
     }
 }
