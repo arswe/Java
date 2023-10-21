f8d18e135aa065b49b52851d9b6b7d5a88cc9af6
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-21 18:39:17 +0600

    Primitive Type Streams

--- Advanced/Streams/PrimitiveTypeStreams/src/Main.java
+++ Advanced/Streams/PrimitiveTypeStreams/src/Main.java
@@ -3,6 +3,9 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) {
         // Primitive Type Streams
-        IntStream.range(1, 10).forEach(System.out::println);
+//        IntStream.range(1, 10).forEach(System.out::println);
+        IntStream.rangeClosed(1, 10).forEach(System.out::println);
+
     }
+
 }
