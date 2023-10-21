ad93da913c5175418646a64a5cece4a6f96f879c
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-21 17:09:52 +0600

    Sorting Streams

--- Advanced/Streams/SortingStreams/src/SortingStreamsDemo.java
+++ Advanced/Streams/SortingStreams/src/SortingStreamsDemo.java
@@ -1,3 +1,4 @@
+import java.util.Comparator;
 import java.util.List;
 
 public class SortingStreamsDemo {
@@ -5,11 +6,17 @@ public class SortingStreamsDemo {
         List<Movie> movies = List.of(
                 new Movie("b", 10),
                 new Movie("c", 15),
-                new Movie("a", 20)
+                new Movie("a", 20),
+                new Movie("d", 25),
+                new Movie("f", 30),
+                new Movie("e", 35)
+
         );
 
         movies.stream()
-                .sorted((a, b) -> a.getTitle().compareTo(b.getTitle()))
+//                .sorted((a, b) -> a.getTitle().compareTo(b.getTitle()))
+                .sorted(Comparator.comparing(Movie::getTitle).reversed())
+                .sorted(Comparator.comparing(Movie::getTitle))
                 .forEach(m -> System.out.println(m.getTitle()));
 
     }
