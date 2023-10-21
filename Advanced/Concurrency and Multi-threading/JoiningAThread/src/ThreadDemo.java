91ad89a5347f5c45c8287420a818bdbfa0102990
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-21 23:24:58 +0600

    Joining a Thread

---
+++ Advanced/Concurrency and Multi-threading/JoiningAThread/src/ThreadDemo.java
@@ -0,0 +1,14 @@
+public class ThreadDemo {
+    public static void show() {
+        Thread thread = new Thread(new DownloadFileTask());
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("File is ready to be scanned: " + Thread.currentThread().getName());
+    }
+}
