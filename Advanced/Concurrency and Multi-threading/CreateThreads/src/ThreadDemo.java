73f82ae88673696d7762304cd16cb3f172469b45
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-21 23:12:35 +0600

    Create Threads

--- Advanced/Concurrency and Multi-threading/CreateThreads/src/ThreadDemo.java
+++ Advanced/Concurrency and Multi-threading/CreateThreads/src/ThreadDemo.java
@@ -2,7 +2,9 @@ public class ThreadDemo {
     public static void show() {
         System.out.println(Thread.currentThread().getName());
 
-        Thread thread = new Thread(new DownloadFileTask());
-        thread.start();
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new DownloadFileTask());
+            thread.start();
+        }
     }
 }
