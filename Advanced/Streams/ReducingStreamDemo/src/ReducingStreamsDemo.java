1551228ba4b09f5020819c49467e066a2f6846aa
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-21 18:01:12 +0600

    Reducing a Stream

--- Advanced/Streams/ReducingStreamDemo/src/ReducingStreamsDemo.java
+++ Advanced/Streams/ReducingStreamDemo/src/ReducingStreamsDemo.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Optional;
 
 public class ReducingStreamsDemo {
     public static void show() {
@@ -7,10 +8,20 @@ public class ReducingStreamsDemo {
                 new Movie("b", 15),
                 new Movie("c", 20)
         );
-        // The following code is equivalent to the code in the previous example
-        movies.stream()
-                .map(Movie::getLikes) // map() takes a Function<T, R> as an argument
-                .reduce(Integer::sum) // reduce() takes a BinaryOperator<T> as an argument
-                .ifPresent(System.out::println); // prints 45
+//        movies.stream()
+//                .map(Movie::getLikes) // map() takes a Function<T, R> as an argument
+//                .reduce(Integer::sum) // reduce() takes a BinaryOperator<T> as an argument
+//                .ifPresent(System.out::println); // prints 45
+
+
+//        Optional<Integer> sum = movies.stream()
+//                .map(Movie::getLikes)
+//                .reduce(Integer::sum);
+//
+//        System.out.println(sum.orElse(0));
+        Integer sum = movies.stream()
+                .map(Movie::getLikes)
+                .reduce(0, Integer::sum);
+        System.out.println(sum);
     }
 }
