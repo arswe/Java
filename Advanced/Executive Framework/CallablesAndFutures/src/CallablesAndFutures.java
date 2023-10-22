3ac4f8ea499a0f101cb7e881f6895ea933ba37f0
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-22 22:21:40 +0600

    Callables and Futures

---
+++ Advanced/Executive Framework/CallablesAndFutures/src/CallablesAndFutures.java
@@ -0,0 +1,25 @@
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
+public class CallablesAndFutures {
+    public static void show() {
+        var executor = Executors.newFixedThreadPool(2);
+
+        try {
+            var future = executor.submit(() -> {
+                LongTask.simulate(); // simulate a long task (3 seconds)
+                return 1;
+            });
+
+            System.out.println("Do more work...");
+            try {
+                var result = future.get(); // blocking call
+                System.out.println(result);
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        } finally {
+            executor.shutdown();
+        }
+    }
+}
