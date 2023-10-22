166c517f3582b1bc77618dbaab7801f24d082579
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-22 09:26:34 +0600

    Locks

---
+++ Advanced/Concurrency and Multi-threading/Locks/src/DownloadFileTask.java
@@ -0,0 +1,22 @@
+public class DownloadFileTask implements Runnable {
+    private DownloadStatus status;
+
+    public DownloadFileTask() {
+        this.status = new DownloadStatus();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Downloading a file: " + Thread.currentThread().getName());
+
+        for (var i = 0; i < 10_000; i++) {
+            if (Thread.currentThread().isInterrupted()) break;
+            System.out.println("Downloading byte " + i);
+        }
+        System.out.println("Download complete: " + Thread.currentThread().getName());
+    }
+
+    public DownloadStatus getStatus() {
+        return status;
+    }
+}
