733a3d1ee81d3348984f95a02fd885f95ba6c77d
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-22 23:32:27 +0600

    Handling Exceptions

---
+++ Advanced/Executive Framework/HandlingExceptions/src/HandlingExceptions.java
@@ -0,0 +1,20 @@
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class HandlingExceptions {
+    public static void show() {
+        var future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Getting the current weather");
+            throw new IllegalStateException();
+        });
+
+        try {
+            var temperature = future.exceptionally(ex -> 1).get();
+            System.out.println(temperature);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
