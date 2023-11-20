8116cd2d72b450719fa395ad8f04e473390fa2d0
Abdur Rahman <abdurrahmansoftw@gmail.com>
2023-11-20 23:38:38 +0600

    New and improved commit message

--- Object-oriented Programming/Interface/TightlyCoupled/src/Main.java
+++ Object-oriented Programming/Interface/TightlyCoupled/src/Main.java
@@ -2,9 +2,5 @@ public class Main {
     public static void main(String[] args) {
         var report = new TaxReport();
         report.show();
-
-        // This is a tightly coupled code because the TaxReport class
-
-
     }
 }
