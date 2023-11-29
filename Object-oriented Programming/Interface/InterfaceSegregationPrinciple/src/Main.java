bc1d32a99df26f8b9f38648511fb97cfd78cb8fc
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-29 22:34:56 +0600

    Refactor

--- Object-oriented Programming/Interface/InterfaceSegregationPrinciple/src/Main.java
+++ Object-oriented Programming/Interface/InterfaceSegregationPrinciple/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Interface Segregation Principle");
+        System.out.println("Hello World!");
     }
 }
