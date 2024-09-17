25f1c5dc52b9a13b70634dbe7bd40703065b2977
arswe <abdurrahmansoftw@gmail.com>
2024-09-17 10:03:41 +0600

    Regular Expression Visit

--- 2024/Fundamentals/RegularExpression/src/Main.java
+++ 2024/Fundamentals/RegularExpression/src/Main.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("w3schools", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher("Visit W3Schools!");
+        Matcher matcher = pattern.matcher("Visit abdurrahman.live!");
         boolean matchFound = matcher.find();
         if(matchFound) {
             System.out.println("Match found");
