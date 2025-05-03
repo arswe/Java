4932b5934b74ebca2f05459123fb2e93a2384466
Abdur Rahman <abdurrahmansoftw@gmail.com>
2025-05-03 10:10:05 +0600

    puski software company

--- 2025/HelloWorld/src/Main.java
+++ 2025/HelloWorld/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         for (int i = 1; i <= 5; i++) {
            System.out.println("i = " + i);
-            System.out.println("Hello puski software company");
+            System.out.println("Hello puski software company agency");
         }
     }
 }
