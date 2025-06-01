89bc7f6fbd55ab6bd189939945a5c988d2122dc3
Abdur Rahman <abdurrahmansoftw@gmail.com>
2025-06-01 21:13:13 +0600

    Fullstack Developers

--- 2025/operator/src/Main.java
+++ 2025/operator/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         char myLetter = 'D';         // character
         boolean myBool = true;       // boolean
 
-        String myText = "Fullstack Software engineer";     // String
+        String myText = "Fullstack Software engineer and Developer";     // String
         System.out.println(myNum);
         System.out.println(myFloatNum);
         System.out.println(myLetter);
